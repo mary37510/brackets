e6f88fc2608e0fc21ddc107ef9a1900a88a438e7
Mary Zhavnerovich <rostova0106@gmail.com>
2020-03-21 13:40:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length;) {
+        let pair = bracketsConfig[i].join('');
+        if (str.includes(pair)) {
+            str = str.replace(pair, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
